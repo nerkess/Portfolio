52f7f0561df312820f0e794f9b958998761b877d
nab <nab@lmis.de>
2020-10-27 11:46:34 +0100

    finishing changes

---
+++ js/animation.js
@@ -0,0 +1,9 @@
+function openMenu(){
+    document.querySelector('.fa').classList.add("close");
+    document.querySelector('.menu-list').classList.add("open");
+}
+
+function closeMenu(){
+    document.querySelector('.fa').classList.remove("close");
+    document.querySelector('.menu-list').classList.remove("open");
+}
